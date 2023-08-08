36db23a89f4c56e0be3c7c938e992bf15861b7b2
drykov-ttc <drycov@gmail.com>
2023-08-08 20:06:23 +0600

    fix

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
 import express from "express";
 import path from "path";
-import fileDirName from "./app/utils/dfn";
+import { URL, fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
 
-const { __dirname } = fileDirName(import.meta);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
+    console.log(req)
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.post("/", (req, res) => {
+    console.log(req)
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
